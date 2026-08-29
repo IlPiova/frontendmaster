f4dd5059fb4cc4f8d7bbaf1f43687bb7dd1b200f
Cristian <piovani.cp@gmail.com>
2026-08-29 19:42:18 +0200

    fix: display score

--- frontend-quiz-app/starter-code/modules/resultsModule.js
+++ frontend-quiz-app/starter-code/modules/resultsModule.js
@@ -2,11 +2,14 @@ let category = sessionStorage.getItem("cat");
 let score = sessionStorage.getItem("score");
 const categoryCard = document.querySelector("#category");
 const result = document.querySelector("#result");
+const button = document.querySelector("#play-again");
 
 function displayScore() {
-  result.innerText = score;
+  result.innerText = `${score}`;
 }
 
-function ctegoryDisplay() {
+function categoryDisplay() {
   categoryCard.innerText = category;
 }
+displayScore();
+categoryDisplay();
