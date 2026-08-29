b7961b8fa3d6b47dc0e7dc1fa5c8294ebaddb395
Cristian <piovani.cp@gmail.com>
2026-08-30 00:21:14 +0200

    style: theme chooser fix: header redirect

--- frontend-quiz-app/starter-code/modules/headerConstructor.js
+++ frontend-quiz-app/starter-code/modules/headerConstructor.js
@@ -4,7 +4,7 @@ const categoryName = document.querySelector("#active-category");
 const themeChooser = document.querySelector("#theme-chooser");
 const sunIcon = document.querySelector("#sun-icon");
 const moonIcon = document.querySelector("#moon-icon");
-const categoryHeader = document.querySelector("#category-header");
+const categoryHeader = document.querySelector("#category-card");
 
 function headerCategoryConstructor() {
   if (categoryName) categoryName.innerText = "";
@@ -47,7 +47,7 @@ function changeTheme() {
     document.documentElement.style.setProperty("color-scheme", "dark");
     sunIcon.setAttribute(
       "src",
-      "/starter-code/assets/images/icon-moon-light.svg",
+      "/starter-code/assets/images/icon-sun-light.svg",
     );
     moonIcon.setAttribute(
       "src",
