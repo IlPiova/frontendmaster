c6a1cec5707f846b70a61187acd5619514364a37
Cristian <piovani.cp@gmail.com>
2026-09-25 20:30:20 +0200

    fix: fetch data path

--- REACT/results-summary-component-main/src/components/SummaryComponent.jsx
+++ REACT/results-summary-component-main/src/components/SummaryComponent.jsx
@@ -6,7 +6,7 @@ function SummaryComponent() {
 
   // Fetching data
   useEffect(() => {
-    fetch("../../public/data.json")
+    fetch("/data.json")
       .then((result) => result.json())
       .then((json) => setData(json));
   }, []);
